0a1f6a5e65ea202ee1a261c3a5b966490d084359
Albin Lindeborg <albin.lindeborg@dynamicdog.se>
2026-03-04 18:30:06 +0100

    auth and logo

--- src/app/users/data.ts
+++ src/app/users/data.ts
@@ -4,3 +4,11 @@ import { db } from '@/lib/db'
 export function getUsers() {
 	return dataFetch(() => db.selectFrom('users').selectAll().execute())
 }
+
+export function getUserByAuth0Id(auth0Id: string) {
+	return db
+		.selectFrom('users')
+		.select(['first_name'])
+		.where('auth0_id', '=', auth0Id)
+		.executeTakeFirst()
+}
---
+++ src/config/logo.ts
@@ -0,0 +1,6 @@
+export const logo = {
+	alt: 'Speedway Fantasy logo',
+	height: 308,
+	width: 1000,
+	src: '/logo-1.png',
+}
